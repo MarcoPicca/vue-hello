cb85b0c86a454d12d0a9a686603f7e0e4e8fad56
MarcoPicca <il96marco@gmail.com>
2023-11-30 15:44:32 +0100

    Vue Hello corretto

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ createApp({
     data() {
         return {
             title: "Se si vede l'immagine allora funziona.",
-            subtitle: "Incredibile !!",
-            extra: "Ora vado a fare il carosello se no mi arrestano!!"
+            
+            
         }
     }
 }).mount('#app');
