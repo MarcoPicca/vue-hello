0ba3ba478acc916998b5b902228038333b0459cd
MarcoPicca <il96marco@gmail.com>
2023-11-30 15:32:47 +0100

    Vue Hello completo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            title: "Se si vede l'immagine allora funziona.",
+            subtitle: "Incredibile !!",
+            extra: "Ora vado a fare il carosello se no mi arrestano!!"
+        }
+    }
+}).mount('#app');
